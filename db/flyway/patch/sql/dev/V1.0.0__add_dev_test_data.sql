1620c905e870bc76a9e2a31caa2f7271e5be167e
Nikita Gorodilov <healyou1994@gmail.com>
2022-03-29 19:05:58 +0300

    Удалено создание файлов в note для dev бд

--- db/flyway/patch/sql/dev/V1.0.0__add_dev_test_data.sql
+++ db/flyway/patch/sql/dev/V1.0.0__add_dev_test_data.sql
@@ -6,10 +6,3 @@ INSERT INTO note (note_guid, text, user_id)
 VALUES ('not guid2', 'note text2', 2);
 INSERT INTO note (note_guid, text, user_id)
 VALUES ('not guid3', 'note text3', 3);
-
-INSERT INTO note_file (id, note_id, file_guid, filename)
-VALUES (1, 1, 'file_guid', 'filename');
-INSERT INTO note_file (id, note_id, file_guid, filename)
-VALUES (2, 2, 'file_guid', 'filename');
-INSERT INTO note_file (id, note_id, file_guid, filename)
-VALUES (3, 3, 'file_guid', 'filename');
